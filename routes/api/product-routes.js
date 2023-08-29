f1c410218d1553613174dd2d0320b22c76fa0ae7
Jason <jasontorrealba199@gmail.com>
2023-08-29 15:07:13 -0400

    Changed alias name

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,7 +7,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category}, { model: Tag, attributes: ['tag_name'], through: ProductTag, as: 'product_tag'}]
+      include: [{ model: Category}, { model: Tag, attributes: ['tag_name'], through: ProductTag, as: 'product_tags'}]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -19,7 +19,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category}, { model: Tag, attributes: ['tag_name'], through: ProductTag, as: 'product_tag'}]
+      include: [{ model: Category}, { model: Tag, attributes: ['tag_name'], through: ProductTag, as: 'product_tags'}]
     });
 
     if (!productData) {
